baa045068ff4b54edec8411373087fa74317a1eb
dwiechert <dan.wiechert@gmail.com>
2015-03-29 20:02:02 -0500

    Parsed user input and added Echo command which echoes out the user's input.
    Closes #2.

--- src/main/scala/com/github/dwiechert/InputArea.scala
+++ src/main/scala/com/github/dwiechert/InputArea.scala
@@ -1,16 +1,30 @@
 package com.github.dwiechert
 
+import java.util.regex.Pattern
+
 import scala.swing.TextArea
 import scala.swing.event.Key
 import scala.swing.event.KeyReleased
+
+import com.github.dwiechert.commands.Echo
 
 class InputArea(lineStart: String) extends TextArea {
   rows = 40
   columns = 60
   append(lineStart)
-  
+
   listenTo(keys)
   reactions += {
-    case KeyReleased(_, Key.Enter, _, _) => append(lineStart)
+    case KeyReleased(_, Key.Enter, _, _) => {
+      val lines = this.peer.getText.split("\n")
+      val line = lines.last.split(Pattern.quote(lineStart))
+      val input = line(1).split(" ")
+      val command = input(0)
+      val arguments = input.drop(1)
+      command match {
+        case "echo" => new Echo(this).run(arguments)
+      }
+      append(lineStart)
+    }
   }
 }
---
+++ src/main/scala/com/github/dwiechert/commands/Command.scala
@@ -0,0 +1,15 @@
+package com.github.dwiechert.commands
+
+import com.github.dwiechert.InputArea
+
+sealed abstract class Command {
+  def run(line: Array[String]): Any
+}
+
+case class Echo(inputArea: InputArea) extends Command {
+  def run(line: Array[String]): Any = {
+    line.foreach { l => inputArea.append(l + " ") }
+    inputArea.append(System.getProperty("line.separator"))
+    None
+  }
+}
